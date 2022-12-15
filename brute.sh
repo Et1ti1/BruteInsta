363a8ffbacf2a0e1a7164c8233616398d2718573
Et1ti1 <114687521+Et1ti1@users.noreply.github.com>
2022-12-15 15:16:31 -0400

    Add files via upload

---
+++ brute.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Script Created By Henry
+apt install python
+clear
+if figlet Henry Brute; then echo "Created by Henry"; fi
+read -p "Continuar com o processo y/n : " node
+if [ $node = "y" ];
+then
+cd insta;
+python insta.py
+elif [ $node = "n" ];
+then
+exit;
+fi
